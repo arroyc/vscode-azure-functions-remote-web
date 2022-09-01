8336b89cdb2aa13b403614d0fbba7ae7496c95ec
peterstone2017 <yunchuwang5@gmail.com>
2022-09-01 18:06:07 -0500

    code refactor

--- docker/api-server/fileUtility.js
+++ docker/api-server/fileUtility.js
@@ -1,15 +1,19 @@
-const { ShareServiceClient, ShareClient } = require("@azure/storage-file-share");
+const {
+  ShareServiceClient,
+  ShareClient,
+} = require("@azure/storage-file-share");
 const axios = require("axios");
 
 class FileManager {
   constructor(connStr, srcBlob, srcURL, shareName) {
     this.srcBlob = srcBlob;
-    this.srcCopyURL = srcURL;   
+    this.srcCopyURL = srcURL;
     const share = new ShareClient(connStr, shareName);
     share.createIfNotExists();
-    this.shareClient = ShareServiceClient.fromConnectionString(
-      connStr
-    ).getShareClient(shareName);
+    this.shareClient =
+      ShareServiceClient.fromConnectionString(connStr).getShareClient(
+        shareName
+      );
   }
 
   async copyZip(dirName) {
@@ -31,29 +35,30 @@ class FileManager {
   async createDirectory(dirName) {
     try {
       // Ex: Deployment/t-tomabraham
-      const path = dirName.split('/');
+      const path = dirName.split("/");
       const directoryClient = this.shareClient.getDirectoryClient(path[0]);
       console.log(
         `Start creating directory ${dirName} at ${new Date().toISOString()}`
       );
-      // Create Deployment directory
       await directoryClient.createIfNotExists();
       console.log(
         `Create directory ${dirName} successfully at ${new Date().toISOString()}`
       );
       for (var i = 1; i < path.length; i++) {
-        // Create t-tomabraham subdirectory under Deployment directory
-        await this.shareClient.getDirectoryClient(path[i - 1]).createSubdirectory(path[i]);
+        // Create subdirectory under parent directory
+        await this.shareClient
+          .getDirectoryClient(path[i - 1])
+          .createSubdirectory(path[i]);
         console.log(
-          `Create subdirectory ${path[i]} successfully at ${new Date().toISOString()}`
+          `Create subdirectory ${
+            path[i]
+          } successfully at ${new Date().toISOString()}`
         );
       }
     } catch (error) {
-      console.log("Directory exists already");
-      
+      //TODO: different error cases: https://docs.microsoft.com/en-us/rest/api/storageservices/create-directory
+      console.log(`Directory ${dirName} exists already`);
     }
-   
-
   }
 
   async deleteZip(dirName) {
@@ -71,34 +76,27 @@ class FileManager {
     }
   }
 
-  async syncCode(dirName) {
+  async copyZipBetweenDirectories(
+    deploymentDirectoryName,
+    stagingDirectoryName
+  ) {
     try {
-      console.log(`Starting syncing code at ${new Date().toISOString()}`);
-      await this.createDirectory(dirName);
-    } catch (error) {
-      console.log(error);
       console.log(
-        dirName +
-          " Folder already exists, overwriting zip to match latest deployment"
+        `Starting copying zip from directory ${deploymentDirectoryName} to directory ${stagingDirectoryName} at ${new Date().toISOString()}`
       );
+      await Promise.all([
+        this.createDirectory(deploymentDirectoryName),
+        this.createDirectory(stagingDirectoryName),
+      ]);
+    } catch (error) {
+      console.log(`Unexpected error when creating directory: ${error}`);
     } finally {
-      await this.copyZip(dirName);
-      console.log(`Done syncing code at ${new Date().toISOString()}`);
+      await this.copyZip(deploymentDirectoryName);
+      console.log(
+        `Done copying function app zip to deployment directory ${deploymentDirectoryName} at ${new Date().toISOString()}`
+      );
     }
   }
-  //     finally {
-  //       const reqBody = {
-  //         stagingDirectoryPath: "/functionapp/Staging",
-  //         zipFileName: "funcapppy.zip",
-  //       };
-  //       console.log(`Hostname: ${hostname} at ${new Date().toISOString()}`);
-  //       // Call staging endpoint here
-  //       await axios.put(`https://${hostname}:443/limelight/staging`, reqBody);
-  //       console.log(
-  //         `${reqBody.zipFileName} has been unzipped at ${reqBody.stagingDirectoryPath}`
-  //       );
-  //     }
-  //   }
 }
 
 module.exports = FileManager;
