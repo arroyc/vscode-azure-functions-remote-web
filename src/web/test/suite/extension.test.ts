e486fcd261dd6d58f92b8fa4f10280b8d7582631
t-tomabraham <t-tomabraham@microsoft.com>
2022-09-09 09:59:19 -0700

    working on testing

--- src/web/test/suite/extension.test.ts
+++ src/web/test/suite/extension.test.ts
@@ -8,8 +8,40 @@ import * as vscode from 'vscode';
 suite('Web Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
+	const workspaceFolder = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0];
+	assert.ok(workspaceFolder, 'Expecting an open folder');
+
+	const workspaceFolderUri = workspaceFolder.uri;
+
+	function getUri(path: string): vscode.Uri {
+		return vscode.Uri.joinPath(workspaceFolderUri, path);
+	}
+
+	async function assertStats(path: string, isFile: boolean, expectedSize?: number) {
+		let stats = await vscode.workspace.fs.stat(getUri(path));
+		assert.deepStrictEqual(stats.type, isFile ? vscode.FileType.File : vscode.FileType.Directory);
+		assert.deepStrictEqual(typeof stats.mtime, 'number');
+		assert.deepStrictEqual(typeof stats.ctime, 'number');
+		if (expectedSize !== undefined) {
+			assert.deepStrictEqual(stats.size, expectedSize);
+		} else {
+			assert.deepStrictEqual(typeof stats.size, 'number');
+		}
+	}
+	async function assertNotExisting(path: string, isFile: boolean) {
+		await assert.rejects(async () => {
+			await assertStats(path, isFile);
+		});
+	}
+
+
+	test('Sample test', async () => {
+		await assertNotExisting('rand/folder', false);
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
+
+	test('test #2', async () => {
+		await assertNotExisting('rand/folder', false);
+	})
 });
